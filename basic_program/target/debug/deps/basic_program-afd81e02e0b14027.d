37bb6d3e184049e8f0343c831cc885cb774cc373
JillStingray1 <87292700+JillStingray1@users.noreply.github.com>
2023-07-30 14:33:50 +0800

    Creating a new crate

---
+++ basic_program/target/debug/deps/basic_program-afd81e02e0b14027.d
@@ -0,0 +1,5 @@
+C:\Users\Jill\Study\Other stuff\Learning Rust\learning-rust\basic_program\target\debug\deps\basic_program-afd81e02e0b14027.rmeta: src\main.rs
+
+C:\Users\Jill\Study\Other stuff\Learning Rust\learning-rust\basic_program\target\debug\deps\basic_program-afd81e02e0b14027.d: src\main.rs
+
+src\main.rs:
---
+++ basic_program/target/debug/deps/basic_program-d720114f6a15ebfe.d
@@ -0,0 +1,5 @@
+C:\Users\Jill\Study\Other stuff\Learning Rust\learning-rust\basic_program\target\debug\deps\basic_program-d720114f6a15ebfe.rmeta: src\main.rs
+
+C:\Users\Jill\Study\Other stuff\Learning Rust\learning-rust\basic_program\target\debug\deps\basic_program-d720114f6a15ebfe.d: src\main.rs
+
+src\main.rs:
