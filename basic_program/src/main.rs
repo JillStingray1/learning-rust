3064cbd732e3aeab9797e5861266c7480e1f6264
JillStingray1 <87292700+JillStingray1@users.noreply.github.com>
2023-08-17 14:23:04 +0800

    Learning stucts in rust

--- basic_program/src/main.rs
+++ basic_program/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    let joe1 = Joe {
+        name: String::from("joe"),
+        age: 42,
+    };
 }
+
+struct Joe {
+    name: String,
+    age: i32,
+}
+impl Joe {}
